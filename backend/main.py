ef983cfbb7f99c937b0ecfec3e4f17231b2654ab
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-10-31 00:55:51 +0530

    First commit

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from models.schemas import TextRequest
+
+
+app = FastAPI(
+    title="MorphNote",
+    description="AI-Assisted Notes App using Generative AI",
+    version="1.0.0"
+)
+
+
